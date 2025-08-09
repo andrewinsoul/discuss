f1640dcaf667cd101b49d0ba925fe99d4ea3c1c7
Nature <andrewinsoul@gmail.com>
2025-08-09 01:15:23 +0100

    add comment channel and add socket connection

--- assets/js/app.js
+++ assets/js/app.js
@@ -72,3 +72,4 @@ liveSocket.connect();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
+import socket from "./user_socket";
